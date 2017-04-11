b1908002bad945a00a9e6f59d78a0bf4bd088c16
Filip Trzaska <filiptrzaska.we@gmail.com>
2017-04-11 18:54:52 +0000

    Username update

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,6 +8,11 @@ class PagesController < ApplicationController
   end
 # back-end code for pages/profile
   def profile
+    if (User.find_by_username(params[:id]))
+    @username = params[:id]
+    else
+      redirect_to root_path, :notice=> "User not found!"
+    end
   end
 # back-end code for pages/explore
   def explore
